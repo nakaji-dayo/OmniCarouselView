c3e9b51a5dbf50ded864df35b0a14f431581bb51
nakaji-dayo <nakaji.dayo@gmail.com>
2016-04-01 00:31:15 +0900

    add image url example

--- Example/OmniCarouselView/ViewController.swift
+++ Example/OmniCarouselView/ViewController.swift
@@ -12,21 +12,25 @@ import OmniCarouselView
 class ViewController: UIViewController {
     @IBOutlet weak var carouselView1: OmniCarouselView!
     @IBOutlet weak var carouselView2: OmniCarouselView!
+    @IBOutlet weak var carouselView3: OmniCarouselView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let images = [UIImage(named: "beer1"), UIImage(named: "beer2"), UIImage(named: "beer3")]
         carouselView1.contents = images
-            .filter({x in x != nil})
+            .filter({$0 != nil})
             .map({OmniCarouselView.Content.Image($0!)})
+        
+        let imageUrls = Array(1...3).map({i in NSURL(string: "https://raw.githubusercontent.com/nakaji-dayo/OmniCarouselView/master/Example/OmniCarouselView/Images.xcassets/beer\(i).imageset/beer\(i).jpeg")})
+        carouselView2.contents = imageUrls.filter({$0 != nil}).map({OmniCarouselView.Content.ImageUrl($0!)})
         
         let labels = Array(0..<3).map { (i) -> UILabel in
             let label = UILabel()
             label.text = "label:\(i)"
             return label
         }
-        carouselView2.contents = labels.map({x in OmniCarouselView.Content.View(x)})
+        carouselView3.contents = labels.map({OmniCarouselView.Content.View($0)})
     }
 
     override func didReceiveMemoryWarning() {
